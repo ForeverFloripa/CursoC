6f870ae3a1db5ba3bc9b9aa36dd669151cc3b946
ForeverFloripa <34176505+ForeverFloripa@users.noreply.github.com>
2021-09-18 21:46:03 -0300

    variaveis

---
+++ .vscode/variaveis/variaveis.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Ola mundo das variaveis");
+    printf("     ");
+
+    int num, num1 = 0;
+
+    double numeroVerificador = 10;
+    return 0;
+}
--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
 int main(){
+    print01();
+    return 0;
+}
+
+void print01(){
     int i =2;
     printf("Hello Word");
+    printf(i);
 }
+
+
+
+
+
