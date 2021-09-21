c91cd76c15931e275ff485e982f852457e8b1aa7
ForeverFloripa <34176505+ForeverFloripa@users.noreply.github.com>
2021-09-21 02:56:51 -0300

    concatenarStrings

---
+++ stringsC/stringC.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+//#include <string.h>
+
+int main(int argc, char const *argv[])
+{
+    /* conceito */
+    char teste[] = {'a', 'n', 'd', 'r', 'e'};
+    int i;
+    for (i = 0; i < 5; i++)
+    {
+        printf("%c", teste[i]);
+        printf("\n");
+    }
+
+    void concatenarStrings(char string1[], int t1, char string2[], int t2,char string3[]);
+
+    char palavra1[] = {'f', 'o', 'r', 'e', 'v', 'e', 'r', ' '};
+    char palavra2[] = {'f', 'l', 'o', 'r', 'i', 'p', 'a'};
+    char novaPalavra[15];
+
+    
+    concatenarStrings(palavra1,8,palavra2,7,novaPalavra);
+
+    int i;
+
+    for (i = 0; i < 15; i++)
+    {
+        printf("%c", novaPalavra[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
+
+void concatenarStrings(char string1[], int t1, char string2[], int t2,char string3[]);
+{
+
+
+}
