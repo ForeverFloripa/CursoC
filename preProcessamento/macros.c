eb8143113934f39f08490c36143ada044e0f89f9
ForeverFloripa <34176505+ForeverFloripa@users.noreply.github.com>
2021-09-24 15:23:29 -0300

    macros

--- preProcessamento/macros.c
+++ preProcessamento/macros.c
@@ -2,21 +2,33 @@
 
 #define PI 3.14159
 #define DOIS_PI 2 * PI
-#define AREA_CIRCULO(raio) raio * raio * PI
+#define AREA_CIRCULO(raio) raio *raio *PI
+#define MAIOR(x, y) x > y ? x : y
+#define E_MINUSCULO(char) char >= 'a' && char <= 'z'
 
 int main(void)
-{   
+{
     float x;
-                printf("digite int: \n");
-                scanf("%f", &x);
+    printf("digite int: \n");
+    scanf("%f", &x);
 
+    printf("%f\n", PI);
+    printf("%f\n", DOIS_PI);
+    printf("%f\n", AREA_CIRCULO(x));
 
-        printf("%f\n",PI);
-        printf("%f\n",DOIS_PI);
-        printf("%f\n",AREA_CIRCULO(x));
+   // scanf("%i", &x);
+    //t2
+    char c = 'A';
+    if (E_MINUSCULO(c))
+    {
+        printf("E'uma letra minuscula\n");
+    }
+    else
+    {
+        printf("Nao e'uma letra minuscula\n");
+    }
 
-    scanf("%i", &x);
+    printf("%i\n", MAIOR(100, 50));
 
-
-     return 0;
+    return 0;
 }
